45a770c1e8e2d0c351b220314c0f676e08cfada5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GridControlSample/SampleSource.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.ObjectModel;
+
+namespace GridControlSample {
+    [POCOViewModel]
+    public class ViewModel {
+        public virtual ObservableCollection<object> Items { get; set; }
+        public ViewModel() {
+            Items = new ObservableCollection<object>();
+            for (int i = 0; i < 30; i++) {
+                SampleItem item = new SampleItem() { Id = i, Name = "Item " + (i % 3).ToString(), Time = DateTime.Now.AddDays(i) };
+                Items.Add(item);
+            }
+        }
+    }
+    public class SampleItem {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ VB/GridControlSample/SampleSource.vb
@@ -0,0 +1,22 @@
+﻿Imports DevExpress.Mvvm.DataAnnotations
+Imports System
+Imports System.Collections.ObjectModel
+
+Namespace GridControlSample
+    <POCOViewModel> _
+    Public Class ViewModel
+        Public Overridable Property Items() As ObservableCollection(Of Object)
+        Public Sub New()
+            Items = New ObservableCollection(Of Object)()
+            For i As Integer = 0 To 29
+                Dim item As New SampleItem() With {.Id = i, .Name = "Item " & (i Mod 3).ToString(), .Time = Date.Now.AddDays(i)}
+                Items.Add(item)
+            Next i
+        End Sub
+    End Class
+    Public Class SampleItem
+        Public Property Id() As Integer
+        Public Property Name() As String
+        Public Property Time() As Date
+    End Class
+End Namespace
